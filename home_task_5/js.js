d6a4f7d5c9bca1443aaab46ef068f6e1d8571cfe
Tataryntseva Svetlana <sitenkosvetlana@gmail.com>
2019-03-13 22:40:24 +0200

    home task 5 done

---
+++ home_task_5/js.js
@@ -0,0 +1,85 @@
+function askNumber(question){ 
+    const value = prompt(question,'');
+
+    const isValidValue = getIsValueValid(value);
+    
+    if (!isValidValue) {
+        alert('invalid value');
+        return askNumber('repeat');
+    } 
+    return +value;
+}
+
+function getIsValueValid(value) {
+
+    const isNanValue = isNaN(value);
+    const isEmptyString = value === '';
+    const wasCanceled = value === null;
+
+    return !(isNanValue || isEmptyString || wasCanceled);
+}
+
+function askOperator(question){ 
+    const operator = prompt(question,'');
+    const isValidOperator = getIsOperatorValid(operator);
+
+    if (!isValidOperator) {
+        alert('invalid operator');
+        return askOperator('repeat');
+    } 
+    return operator;
+}
+
+function getIsOperatorValid(operator) {
+
+    const arr = ['+','-','*','/'];
+
+    for (let i = 0; i < arr.length; i++){
+        if (operator === arr[i]) { 
+            return true;
+        } 
+    }
+    return false;
+}
+
+function validateCalculation (operand, operator) {
+
+    const isDevidedByZero = getIsDividedbyZero(operand, operator);
+
+    if (isDevidedByZero) {
+        alert('Cannot divide by zero');
+        
+        secondUserValue = askNumber('enter the second number');
+        
+        if (secondUserValue === 0) {
+            validateCalculation (secondUserValue, operator);
+        }
+    }
+}
+
+function getIsDividedbyZero (operand, operator){
+    return operand === 0 && operator === '/';
+}
+
+function calculate(operandA, operandB, operator) {
+
+    switch (operator) {
+        case '+': return operandA + operandB;
+        case '-': return operandA - operandB;
+        case '*': return operandA * operandB;
+        case '/': return operandA / operandB; 
+    }
+}
+
+const firstUserValue = askNumber('enter the first number');
+
+let secondUserValue = askNumber('enter the second number');
+
+const userOperator = askOperator('enter the operator, example: "+" , "-", "*", "/" ');
+
+validateCalculation (secondUserValue, userOperator);
+
+const result = calculate(firstUserValue, secondUserValue, userOperator);
+alert(result);
+
+
