88d3bc6abe3d536a4e49b19bf2d826fcba7fcd0e
Tataryntseva Svetlana <sitenkosvetlana@gmail.com>
2019-04-03 08:35:22 +0300

    Home task 11

---
+++ home_task_11/app.js
@@ -0,0 +1,42 @@
+// Функция calculator
+// Написать функцию calculator, которую можно будет использовать следующим образом
+
+// const value = calculator(10);
+
+// value.add(45) // возвращает 55
+// value.sub(45) // возвращает -35
+// value.divide(5) // возвращает 2
+// value.mult(5) // возвращает 50
+// value.set(100) // устанавливает базовое значение в 100
+// value.get() // возвращает базовое значение (в данный момент 100)
+
+// value.mult(5) // возвращает 500
+
+function calculator(number) { 
+    let primaryValue = number;
+
+    return {
+        add: function(value) {
+            return result = primaryValue + value;
+        },  
+        sub: function(value) {
+            return result = primaryValue - value;
+        },
+        divide: function(value) {
+            return result = primaryValue / value;
+        },
+        mult: function(value) {
+            return result = primaryValue * value;
+        },  
+        set: function(value) {
+            primaryValue = value;
+        },
+        get: function() {
+            return result = primaryValue;
+        }
+    }
+}
+
+const value = calculator(20);
+
+
